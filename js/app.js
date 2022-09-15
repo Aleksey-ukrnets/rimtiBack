a790a43fc133e3aebffd2c271ac530d19a4fd339
Petro Pasteruk <57048376+Petro-Pasteruk@users.noreply.github.com>
2022-09-15 15:19:32 +0300

    Add fetch

--- js/app.js
+++ js/app.js
@@ -3,22 +3,51 @@ const email = document.querySelector('#email');
 const text = document.querySelector('.subscribe__checking');
 const button = document.querySelector('.btn');
 
-const validateEmail= (email) => {
+const validateEmail = (email) => {
     const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
 }
 
-button.addEventListener('click',(e)=> {
+button.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if(validateEmail(email.value)){
+    if (validateEmail(email.value)) {
         form.classList.add('valid');
         form.classList.remove('invalid');
+
+
+        const
+            token = "tfp_CBLDQjShKZM8VSZaTMWqDB6VMvL7w3Y8C3kebKWj9CC8_3spqGWJan4RQUG",
+            formKey = "zxxER2SK",
+            myHeaders = new Headers();
+
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Access-Control-Allow-Origin", "*");
+        // myHeaders.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        // myHeaders.append("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+        // myHeaders.append('Access-Control-Allow-Credentials', 'true');
+        myHeaders.append("Authorization", "Bearer tfp_CBLDQjShKZM8VSZaTMWqDB6VMvL7w3Y8C3kebKWj9CC8_3spqGWJan4RQUG");
+        // myHeaders.append("Cookie", "AWSALBTG=IMbb8H6h5Oh96DogpzPAk02xAZ8eqZM6E2FTFBI/LZncXDt2iAuyVJwlBOFUZqhWNurlI4QZbiOENf6AWemhRGeAphBHi2Qbo7EpfSiYAdCAoToH5DS4pPIjiXLGaE8taNTG2sSzdFiuNxp0J9O3NWgK1k2xwVMIZYLhYoLMHOIn; AWSALBTGCORS=IMbb8H6h5Oh96DogpzPAk02xAZ8eqZM6E2FTFBI/LZncXDt2iAuyVJwlBOFUZqhWNurlI4QZbiOENf6AWemhRGeAphBHi2Qbo7EpfSiYAdCAoToH5DS4pPIjiXLGaE8taNTG2sSzdFiuNxp0J9O3NWgK1k2xwVMIZYLhYoLMHOIn");
+
+        const requestOptions = {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("https://api.typeform.com/forms/zxxER2SK", requestOptions)
+            .then(response => response)
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+
         text.innerHTML = 'Success! You’re on the waitlist!';
         text.style.color = "#C6FF23";
         email.style.border = "border: 1px solid #0B304B"
         email.value = '';
-    }else{
+    } else {
         form.classList.remove('valid');
         form.classList.add('invalid');
         text.innerHTML = 'Check your email address';
